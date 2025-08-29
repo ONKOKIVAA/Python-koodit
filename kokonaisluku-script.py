5289f19576c0de9ca2eeb878e27cf10fc58fd841
ONKOKIVAA <miikka.paasikoski@gmail.com>
2025-08-29 18:28:38 +0300

    lisää tehtäviä

---
+++ kokonaisluku-script.py
@@ -0,0 +1,12 @@
+# p = a + b + c
+luku = float (input("anna kokonaisluku "))
+luku2 = float(input("anna luvun kaksi kokonaisluku"))
+luku3 = float(input("anna luvun kolme kokonaisluku"))
+summa = luku + luku2 + luku3
+tulo = luku * luku2 * luku3
+keskiarvo = summa / 3
+print("summa on : " + str(summa))
+print("lukujesi tulo on  : " + str(tulo))
+print ("keskiarvo on : " + str(keskiarvo))
+
+
